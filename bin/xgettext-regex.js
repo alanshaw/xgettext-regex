7fe88b59830288daad38373503819ee1068b0869
Alan Shaw <alan@tableflip.io>
2014-12-10 08:48:59 +0000

    Add version, help and fn args to bin

--- bin/xgettext-regex.js
+++ bin/xgettext-regex.js
@@ -4,20 +4,34 @@ var argv = require('minimist')(process.argv.slice(2))
 var path = require('path')
 var xgettext = require('../')
 
-var outFile = argv.o || argv.output
+if (argv.v || argv.version) {
+  return console.log(require('../package.json').version)
+}
+
+if (argv.h || argv.help) {
+  return fs.createReadStream(__dirname + '/usage.txt').pipe(process.stdout)
+}
+
+var outFile = argv.o || argv.outfile
+var opts = {
+  fn: (argv.f || argv.fn)
+}
 
 if (argv._.length) {
   var files = argv._.map(function (filename) { return path.resolve(filename) })
-  var readable = xgettext.createReadStream(files)
+  var readable = xgettext.createReadStream(files, opts)
+
   if (outFile) {
     readable.pipe(fs.createWriteStream(outFile))
   } else {
     readable.pipe(process.stdout)
   }
 } else {
+  var duplex = xgettext('process.stdin', opts)
+
   if (outFile) {
-    process.stdin.pipe(xgettext()).pipe(fs.createWriteStream(outFile))
+    process.stdin.pipe(duplex).pipe(fs.createWriteStream(outFile))
   } else {
-    process.stdin.pipe(xgettext()).pipe(process.stdout)
+    process.stdin.pipe(duplex).pipe(process.stdout)
   }
 }
